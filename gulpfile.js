b1f1bc0e8254e429e242fa5bdf2a482c4ae6b960
furlan <furlan@cromatic.com.br>
2017-08-23 16:56:31 -0300

    finalização do header animado

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,12 @@
 var gulp    = require('gulp')
-    , sass  = require('gulp-sass');
+  , sass  = require('gulp-sass');
 
 gulp.task('sass', function(){
-    gulp.src('./src/sass/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('./src/css/'));
-})
-
+  gulp.src('./src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./src/css/'));
+});
 
 gulp.task('listen', function(){
-    gulp.watch('./src/sass/**/*.scss', ['sass'])
+  gulp.watch('./src/sass/**/*.scss', ['sass'])
 })
