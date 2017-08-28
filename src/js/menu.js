1efc090371aaf4000bf5ffd4f34f48c039787aa9
furlan <furlan@cromatic.com.br>
2017-08-28 11:21:27 -0300

    Menu nav: acerto na acessibilidade e ajustes no CSS do hover deskot

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,6 +3,7 @@
 
   var btn = document.querySelector('.header-nav__hamburgger');
   var html = document.querySelector('html');
+  var menu = document.querySelector('#mainMenu');
   var ClassMenu = 'menu-opened';
   var menuOpened = false;
 
@@ -25,10 +26,14 @@
     menuOpened = false;
     html.classList.remove(ClassMenu);
     btn.blur();
+    menu.setAttribute('aria-expanded', false);
+    btn.setAttribute('aria-expanded', false);
   }
   function openMenu() {
     menuOpened = true;
     html.classList.add(ClassMenu);
+    menu.setAttribute('aria-expanded', true);
+    btn.setAttribute('aria-expanded', true);
   }
 
 }())
