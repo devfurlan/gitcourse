62204a2d18f9759645e62ea635f0ed3a8eceaed5
furlan <furlan@cromatic.com.br>
2017-08-28 11:44:28 -0300

    inclusão da função gul-file-include

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp    = require('gulp'),
     sass  = require('gulp-sass'),
+    include  = require('gulp-file-include'),
     browserSync = require('browser-sync');
 
 gulp.task('sass', function(){
   gulp.src('./src/sass/**/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('./src/css/'));
+});
+
+gulp.task('html', function(){
+  gulp.src('./src/index.html')
+    .pipe(include())
+    .pipe(gulp.dest('./dist/'))
 });
 
 gulp.task('server', function(){
